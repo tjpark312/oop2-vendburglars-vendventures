30a82c0d6d22aebe142f48d79f3a556c266e3d02
jectgenius <jkl9087@naver.com>
2024-04-30 17:04:40 +0900

    Feat: update Manager 클래스 #3

--- src/main/java/entity/Manager.java
+++ src/main/java/entity/Manager.java
@@ -1,4 +1,34 @@
 package entity;
 
 public class Manager {
+    private String email;
+    private String password;
+    private String name;
+
+    // 싱글톤 패턴??
+    // 생성자 생성하기
+    public Manager(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    // getter/setter 생성하기
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getName() {
+        return this.name; }
 }
