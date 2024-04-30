64cfb639e48aeb4e6b61f1f54f5a6531ecc9c88d
jectgenius <jkl9087@naver.com>
2024-04-30 21:39:55 +0900

    Feat: add ManagerController의 logIn() 메소드 #9

--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import repository.ManagerRepository;
+
 public class ManagerController {
+    private ManagerRepository managerRepository = new ManagerRepository();
+
+    public void logIn(String email, String password) {
+        managerRepository.logIn(email, password);
+    }
 }
